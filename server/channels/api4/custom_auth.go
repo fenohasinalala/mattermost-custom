76ba2ed1959a2c0c39d8cb12630a0c2f1df9744f
Lova Fenohasina Lala RAFANOMEZANTSOA <fenohasinalala@gmail.com>
2024-09-12 01:18:42 +0300

    feat: token generation

--- server/channels/api4/custom_auth.go
+++ server/channels/api4/custom_auth.go
@@ -4,13 +4,34 @@
 package api4
 
 import (
+	"encoding/json"
 	"fmt"
+	"github.com/casdoor/casdoor-go-sdk/casdoorsdk"
 	"net/http"
 )
 
 func (api *API) InitCustomAuth() {
 	api.BaseRoutes.CustomAuth.Handle("/ping", api.APIHandler(getCustomAuthPing)).Methods(http.MethodGet)
-	//api.BaseRoutes.CustomAuth.Handle("/signin", api.APIHandler(signinHandler)).Methods(http.MethodPost)
+	api.BaseRoutes.CustomAuth.Handle("/signin", api.APIHandler(signinHandler)).Methods(http.MethodPost)
+}
+
+func signinHandler(c *Context, w http.ResponseWriter, r *http.Request) {
+	code := r.URL.Query().Get("code")
+	state := r.URL.Query().Get("state")
+
+	token, err := casdoorsdk.GetOAuthToken(code, state)
+	if err != nil {
+		fmt.Println("GetOAuthToken() error", err)
+		http.Error(w, "GetOAuthToken() error", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(map[string]interface{}{
+		"status": "ok",
+		"data":   token.AccessToken,
+	})
 }
 
 func getCustomAuthPing(c *Context, w http.ResponseWriter, r *http.Request) {
