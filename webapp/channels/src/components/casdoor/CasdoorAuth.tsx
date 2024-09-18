386e6b3240201fced129fac5bbc211b01ece0d02
Lova Fenohasina Lala RAFANOMEZANTSOA <fenohasinalala@gmail.com>
2024-09-18 14:08:19 +0300

    fixUp: casdoor login unauthorized

--- webapp/channels/src/components/casdoor/CasdoorAuth.tsx
+++ webapp/channels/src/components/casdoor/CasdoorAuth.tsx
@@ -18,7 +18,7 @@ import {trackEvent} from 'actions/telemetry_actions';
 import type {CustomizeHeaderType} from 'components/header_footer_route/header_footer_route';
 
 import {
-    ServerUrl,
+    ServerUrl, goToLink
 } from './Setting';
 
 import {redirectUserToDefaultTeam} from '../../actions/global_actions';
@@ -70,59 +70,6 @@ const AuthCallback = ({
         }).then((res) => res.json());
     };
 
-    /*
-    async function loginToMattermost(loginId: string, password: string, token: string = '', deviceId: string = ''): Promise<any> {
-        const url = 'https://mattermost.numer.tech/api/v4/users/login';
-
-        const headers: HeadersInit = {
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:130.0) Gecko/20100101 Firefox/130.0',
-
-            'Accept-Language': 'en',
-            'Accept-Encoding': 'gzip, deflate, br, zstd',
-            'X-Requested-With': 'XMLHttpRequest',
-            'Content-Type': 'application/json',
-
-            //'Origin': 'https://mattermost.numer.tech',
-            //'DNT': '1',
-            'Sec-GPC': '1',
-
-            //'Connection': 'keep-alive',
-            //'Cookie': 'rl_anonymous_id=RudderEncrypt%3AU2FsdGVkX1%2BTLWa%2FpAJAxVzkqSz2so9xmHj4qPjnkhvQsT%2F8%2FXbbMlSkAFEFM3MQan%2F4ouEQKsCw6AxkAZ5MtA%3D%3D; rl_user_id=RudderEncrypt%3AU2FsdGVkX19ZneNXXCyPzrwuRekjx3Eta8XfXYTaiZAo%2B6%2FGBN%2BE7l%2B48var9Yvk;',
-            'Sec-Fetch-Dest': 'empty',
-            'Sec-Fetch-Mode': 'cors',
-            'Sec-Fetch-Site': 'same-origin',
-
-            //'Priority': 'u=0',
-            //'TE': 'trailers'
-        };
-
-        const body = JSON.stringify({
-            login_id: loginId,
-            password,
-            token,
-            deviceId,
-        });
-
-        try {
-            const response = await fetch(url, {
-                method: 'POST',
-                headers,
-                body,
-            });
-
-            if (!response.ok) {
-                throw new Error(`HTTP error! Status: ${response.status}`);
-            }
-
-            const data = await response.json();
-            console.log('Login successful:', data);
-            return data;
-        } catch (error) {
-            console.error('Error logging in:', error);
-            throw error;
-        }
-    }
-     */
 
     useEffect(() => {
         const urlParams = new URLSearchParams(window.location.search);
@@ -130,13 +77,9 @@ const AuthCallback = ({
         const state = urlParams.get('state');
         handleCallback(code, state).then((res) => {
             if (res?.status === 'ok') {
-                //loginToMattermost('', '', res.data, '');
-
-                //TODO: send token
-                //submit({loginId: 'fenohasinalala@gmail.com', password: '00000000', token: ''});
                 submit({loginId: '', password: '', token: res.data});
             } else {
-                //console.error('Error logging in:', res?.status);
+                //console.erroddr('Error logging in:', res?.status);
             }
         });
     }, [location]);
@@ -203,6 +146,8 @@ const AuthCallback = ({
                 });
                 setHasError(true);
             }
+        //TODO: display : Casdoor user not authorized --> with confirmation button --> redirection to login page
+        goToLink("/")
             return;
         }
 
